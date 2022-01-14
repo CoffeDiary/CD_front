3943ac84efe92d021cde7b736c77ca36633b34f9
cjang <cjang@student.42seoul.kr>
2022-01-15 03:13:58 +0900

    HOME 캘린더 반영

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,6 @@
     </p>
     <v-calendar
       class="custom-calendar max-w-full"
-      :masks="masks"
       :attributes="attributes"
       disable-page-swipe
       is-expanded
@@ -25,7 +24,6 @@
               v-for="attr in attributes"
               :key="attr.key"
               class="text-xs leading-tight rounded-sm p-1 mt-0 mb-1"
-              :class="attr.customData.class"
             >
               {{ attr.customData.title }}
             </p>
@@ -37,92 +35,44 @@
 </template>
 
 <script>
+import { getDiaryList } from '../../api';
+const _ = require("lodash");
 export default {
   name: "Home",
   data() {
-    const month = new Date().getMonth();
-    const year = new Date().getFullYear();
     return {
-      title: "Coffee Diary",
-      masks: {
-        weekdays: 'WWW',
+      diarylist: {
+        key: Number,
+        title: String,
+        customData: {
+          title: String,
+        },
+        dates: Date,
       },
-      attributes: [
-        {
-          key: 1,
-          customData: {
-            title: 'Lunch with mom.',
-            class: 'bg-red-600 text-white',
-          },
-          dates: new Date(year, month, 1),
-        },
-        {
-          key: 2,
-          customData: {
-            title: 'Take Noah to basketball practice',
-            class: 'bg-blue-500 text-white',
-          },
-          dates: new Date(year, month, 2),
-        },
-        {
-          key: 3,
-          customData: {
-            title: "Noah's basketball game.",
-            class: 'bg-blue-500 text-white',
-          },
-          dates: new Date(year, month, 5),
-        },
-        {
-          key: 4,
-          customData: {
-            title: 'Take car to the shop',
-            class: 'bg-indigo-500 text-white',
-          },
-          dates: new Date(year, month, 5),
-        },
-        {
-          key: 4,
-          customData: {
-            title: 'Meeting with new client.',
-            class: 'bg-teal-500 text-white',
-          },
-          dates: new Date(year, month, 7),
-        },
-        {
-          key: 5,
-          customData: {
-            title: "Mia's gymnastics practice.",
-            class: 'bg-pink-500 text-white',
-          },
-          dates: new Date(year, month, 11),
-        },
-        {
-          key: 6,
-          customData: {
-            title: 'Cookout with friends.',
-            class: 'bg-orange-500 text-white',
-          },
-          dates: { months: 5, ordinalWeekdays: { 2: 1 } },
-        },
-        {
-          key: 7,
-          customData: {
-            title: "Mia's gymnastics recital.",
-            class: 'bg-pink-500 text-white',
-          },
-          dates: new Date(year, month, 22),
-        },
-        {
-          key: 8,
-          customData: {
-            title: 'Visit great grandma.',
-            class: 'bg-red-600 text-white',
-          },
-          dates: new Date(year, month, 25),
-        },
-      ]
+      title: "Coffee Diary",
+      attributes: [],
     }
   },
+  async mounted() {
+    const userData = '';
+    try {
+      const response = await getDiaryList(userData);
+      // this.diarylist = response.data;
+      for (var i = 0; i < response.data.length; i++) {
+        this.diarylist.key = i;
+        this.diarylist.customData = {title: response.data[i].title};
+        console.log(response.data[i].title.slice(0));
+        var d = response.data[i].modifiedDate;
+        this.diarylist.dates = new Date(d[0], d[1]-1, d[2], d[3], d[4], d[5]);
+        this.attributes[i] = _.clone(this.diarylist);
+      }
+      console.log(this.attributes);
+    }
+    catch (error) {
+      console.log(error.response.data);
+      this.logMessage = error.response.data;
+    }
+  }
 };
 </script>
 
