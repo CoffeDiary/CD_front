da1ce596a2a1d9a247fe38818a507bb85fbbe36c
cjang <cjang@student.42seoul.kr>
2022-01-14 22:22:05 +0900

    view에서 대문자 파일들  다시 추가

---
+++ src/views/login/Post.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="submitForm">
+      <div class="mb-3">
+        <label for="username" class="form-label">username</label>
+        <input type="text" id="username" class="form-control" v-model="username" />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">password</label>
+        <input type="password" id="password" class="form-control" v-model="password" />
+      </div>
+      <button type="submit" class="btn btn-primary">Signin</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { loginUser } from "../../../api";
+export default {
+  name: 'SigninForm',
+  data() {
+    return {
+      password: '',
+      username: '',
+    };
+  },
+  methods: {
+    async submitForm() {
+      const userData = {
+        password: this.password,
+        userName: this.username,
+      };
+      const response = await loginUser(userData);
+      if (response.status == 200) {
+        alert('Welcome to Coffee Diary!');
+        this.$cookies.set("access_token", response.data);
+        this.emitter.emit('logged');
+        this.$router.push('/home');
+      }
+      else {
+        alert('Fail');
+        alert(response.data);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
